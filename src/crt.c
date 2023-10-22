9d8bede0503d03ac0616f7a6971fa61dc3dc4cbf
giomba <giomba@glgprograms.it>
2023-10-22 11:40:28 +0200

    Add method to wait for some frames in CRT.

--- src/crt.c
+++ src/crt.c
@@ -1,10 +1,13 @@
 #include "crt.h"
 
-#include <stdint.h>
-
 void crt_waitNextFrame(void) {
     void (*frame_sync_detection_routine)(void) = (void *)0xc75b;
     __asm__("push af");
     frame_sync_detection_routine();
     __asm__("pop af");
+}
+
+void crt_waitFrames(uint16_t n) {
+    for (uint16_t i = 0; i < n; ++i)
+        crt_waitNextFrame();
 }
--- src/crt.h
+++ src/crt.h
@@ -1,6 +1,10 @@
 #ifndef CEDA_CRT_H
 #define CEDA_CRT_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 void crt_waitNextFrame(void);
+void crt_waitFrames(uint16_t n);
 
 #endif // CEDA_CRT_H
