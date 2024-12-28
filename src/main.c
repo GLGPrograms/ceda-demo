0d9b24a4235ef84f43d54e8f3cd2a6e97bdba827
giomba <giomba@glgprograms.it>
2024-12-28 11:49:01 +0100

    Add infrastructure to enable/disable demo parts at runtime.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "cge.h"
 #include "compiler.h"
 #include "crt.h"
 #include "cursor.h"
@@ -9,20 +10,20 @@
 #include "matrix.h"
 #include "mmap.h"
 #include "printf.h"
+#include "snow.h"
 #include "video.h"
-
-static void wait(void) {
-    (void)io_in(0xA0);
-
-    for (;;) {
-        const uint8_t port_c = io_in(0x82);
-        if (port_c & 0x2)
-            break;
-    }
-}
 
 void _putchar(char c) {
     video_putchar(c);
+}
+
+static void *_memset(void *s, int c, size_t n) {
+    char *start = (char *)s;
+    char *end = (char *)s + n;
+    char value = (char)c;
+    for (char *ptr = start; ptr != end; ++ptr)
+        *ptr = value;
+    return s;
 }
 
 const uint8_t GLG[28 * 8] = {
@@ -45,59 +46,83 @@ const uint8_t FVB[15 * 8] = {
     1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0,
 };
 
+enum demo_part_t {
+    DEMO_INTRO,
+    DEMO_FVB,
+    DEMO_MATRIX,
+    DEMO_CURSOR,
+    DEMO_CGE,
+
+    DEMO_COUNT,
+};
+
+static bool demo_enable[DEMO_COUNT] = {false};
+
 int main(void) {
-    mmap_set(MMAP_MODE_CEDA_VIDEO);
+    _memset(&demo_enable, 1, sizeof(demo_enable));
+
+    cursor_enable(false);
+    video_cls();
 
     for (;;) {
-        cursor_enable(false);
-        video_cls();
+        if (demo_enable[DEMO_INTRO]) {
+            flipflap_init(GLG, 28, 8, 8, 26);
+            while (!flipflap_finished()) {
+                flipflap_poll();
+                crt_waitNextFrame();
+            }
+            video_enableHorizontalStretch(true);
+            video_enableVerticalStretch(true);
+            video_locate(18, 15);
+            printf("RetrOfficina GLG Programs");
+            video_enableHorizontalStretch(false);
+            video_enableVerticalStretch(false);
+            video_locate(20, 22);
+            printf("http://retrofficina.glgprograms.it/");
 
-        flipflap_init(GLG, 28, 8, 8, 26);
-        while (!flipflap_finished()) {
-            flipflap_poll();
-            crt_waitNextFrame();
-        }
-        video_enableHorizontalStretch(true);
-        video_enableVerticalStretch(true);
-        video_locate(18, 15);
-        printf("RetrOfficina GLG Programs");
-        video_enableHorizontalStretch(false);
-        video_enableVerticalStretch(false);
-        video_locate(20, 22);
-        printf("http://retrofficina.glgprograms.it/");
-
-        crt_waitFrames(250);
-        video_cls();
-        crt_waitFrames(100);
-
-        flipflap_init(FVB, 15, 8, 2, 40 - 15 / 2);
-        while (!flipflap_finished()) {
-            flipflap_poll();
-            crt_waitNextFrame();
+            crt_waitFrames(250);
         }
 
-        video_enableHorizontalStretch(true);
-        video_enableVerticalStretch(true);
-        video_locate(10, 21);
-        printf("Firenze Vintage Bit");
-        video_locate(12, 36);
-        printf("2023");
+        if (demo_enable[DEMO_FVB]) {
+            video_cls();
+            crt_waitFrames(100);
+            flipflap_init(FVB, 15, 8, 2, 40 - 15 / 2);
+            while (!flipflap_finished()) {
+                flipflap_poll();
+                crt_waitNextFrame();
+            }
 
-        crt_waitFrames(250);
-        video_cls();
-        crt_waitFrames(100);
+            video_enableHorizontalStretch(true);
+            video_enableVerticalStretch(true);
+            video_locate(10, 21);
+            printf("Firenze Vintage Bit");
+            video_locate(12, 36);
+            printf("2023");
 
-        const uint8_t trails[] = {5, 10, 20, 50};
-        for (uint8_t t = 0; t < countof(trails); ++t) {
-            matrix_init(trails[t]);
-            for (size_t f = 0; f < 120; ++f) {
-                matrix_poll();
-                crt_waitFrames(2);
+            crt_waitFrames(250);
+        }
+
+        if (demo_enable[DEMO_MATRIX]) {
+            video_cls();
+            crt_waitFrames(100);
+            const uint8_t trails[] = {5, 10, 20, 50};
+            for (uint8_t t = 0; t < countof(trails); ++t) {
+                matrix_init(trails[t]);
+                for (size_t f = 0; f < 120; ++f) {
+                    matrix_poll();
+                    crt_waitFrames(2);
+                }
             }
         }
 
-        crt_waitFrames(100);
+        if (demo_enable[DEMO_CURSOR]) {
+            crt_waitFrames(100);
+            magicCursor_run();
+            crt_waitFrames(100);
+        }
 
-        magicCursor_run();
+        if (demo_enable[DEMO_CGE]) {
+            cge_run();
+        }
     }
 }
